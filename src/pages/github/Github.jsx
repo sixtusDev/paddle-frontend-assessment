5151d3bbb4a2ad57ff0d4b742cc40b0a2581ac8c
Sixtus Innocent <sixtusinno@gmail.com>
2021-10-23 02:45:50 +0100

    Action: added markup and styles for the github most starred repos on the view

--- src/pages/github/Github.jsx
+++ src/pages/github/Github.jsx
@@ -6,21 +6,70 @@ import "./Github.scss";
 const Github = () => {
   const [mostStarredRepos, setMostStarredRepos] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const fetchMostStarredRepos = async () => {
       try {
+        setLoading(true);
         const { data } = await getMostStarredRepos();
+        console.log(data);
         setMostStarredRepos(data.items);
       } catch (error) {
         console.log(error);
       } finally {
+        setLoading(false);
       }
     };
     fetchMostStarredRepos();
-  });
+  }, []);
   return (
     <div className="Github">
-      <span>Hello</span>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        mostStarredRepos.length &&
+        mostStarredRepos.map(
+          ({
+            owner,
+            description,
+            name,
+            stargazers_count,
+            open_issues_count,
+          }) => (
+            <div className="Github__card-wrapper">
+              <Card className="Github__card">
+                <div className="flex-row align-items-center">
+                  <div className="mr20">
+                    <img
+                      className="Github__card-img"
+                      alt="avatar"
+                      src={owner.avatar_url}
+                    />
+                  </div>
+                  <div className="flex-column">
+                    <span className="app-heading2">{name}</span>
+                    <span className="app-paragraph1 mb10">
+                      {description || "There is no description for this repo"}
+                    </span>
+                    <div className="flex-row align-items-center">
+                      <div className="mr10">
+                        <span className="badge mr10">
+                          Stars: {stargazers_count}
+                        </span>
+                        <span className="badge">
+                          Issues: {open_issues_count}
+                        </span>
+                      </div>
+                      <span>Submitted 30 days ago by {owner.login}</span>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          )
+        )
+      )}
     </div>
   );
 };
